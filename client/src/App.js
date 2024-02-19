02991b54d37072f717802d3c6a9e125764f170a1
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-02-20 00:27:28 +0100

    trying to fix error

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import  HomePage from './components/homePage.jsx';
+
+const  App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
---
+++ client/src/components/homePage.jsx
@@ -0,0 +1,11 @@
+
+
+function homePage() {
+    return (
+        <div className="homePage">
+            <h1>Welcome to our website!</h1>
+        </div>
+    );
+};
+
+export default homePage;
