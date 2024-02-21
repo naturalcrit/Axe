6e21754070963802801e315d793afd0b63de1085
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-02-21 22:04:23 +0100

    functional website

---
+++ src/components/draggables/statInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StatInput = () => {
+  return (
+      <label>
+        Ability
+        <input type="number" name="ability"/>
+      </label>
+  );
+};
+
+export default StatInput;
