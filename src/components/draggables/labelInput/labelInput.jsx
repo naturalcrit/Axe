3141f4e5c157e7ca497f158b7a56ea569d2590ce
Gazook89 <John.W.Jones89@gmail.com>
2024-03-02 22:21:39 -0600

    set CSS on labelInput component

--- src/components/draggables/labelInput/labelInput.jsx
+++ src/components/draggables/labelInput/labelInput.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './labelInput.css';
 
 const LabelInput = () => {
   return (
+    <div className='label-input'>
       <label>
         Character Name
         <input type="text" name="name" />
       </label>
+    </div>
   );
 };
 
