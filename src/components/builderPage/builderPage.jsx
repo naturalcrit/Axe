ad30bf4b4e32100dab69d7447391f975bbacb07e
Gazook89 <John.W.Jones89@gmail.com>
2024-03-02 23:53:07 -0600

    Fix Delete Item button
    adds `onMouseDown={(event) => event.stopPropagation()}` to delete item button which prevents a mouseDown event from triggering a drag-n-drop routine before firing the click event.

--- src/components/builderPage/builderPage.jsx
+++ src/components/builderPage/builderPage.jsx
@@ -182,6 +182,7 @@ class Builder extends Component {
                             <button
                                 className="deleteItem"
                                 onClick={() => this.deleteItem(item.i)}
+                                onMouseDown={(event) => event.stopPropagation()}
                             >
                                 x
                             </button>
