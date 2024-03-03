bfc554bbeb347a2461e8e7e16d3b085f031c343e
Gazook89 <John.W.Jones89@gmail.com>
2024-03-02 23:01:26 -0600

    add textArea css

---
+++ src/components/draggables/textArea/textarea.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './textarea.css';
+
+
+const LabelInput = () => {
+  return (
+    <div className='text-area-input'>
+      <label>
+        Text Area
+        <textarea name="" id="" cols="30" rows="1"></textarea>
+      </label>
+    </div>
+  );
+};
+
+export default LabelInput;
