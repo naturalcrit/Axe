783aec9aa62e4aa1c4bbf286669804b5626e3563
Gazook89 <John.W.Jones89@gmail.com>
2024-03-03 00:32:55 -0600

    Use input fields as labels.

--- src/components/draggables/labelInput/labelInput.jsx
+++ src/components/draggables/labelInput/labelInput.jsx
@@ -3,11 +3,11 @@ import './labelInput.css';
 
 const LabelInput = () => {
   return (
-    <div className='label-input'>
-      <label>
-        Character Name
-        <input type="text" name="name" size="1" />
-      </label>
+    <div className='label-input'>   
+      <div className='label'>
+        <input type="text" size="1" aria-label='property name' placeholder='Character Name'/>
+        <input type="text" size="1" aria-label='property value' />
+      </div>    
     </div>
   );
 };
