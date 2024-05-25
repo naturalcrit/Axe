8b2b85cb2d11f80061679ae36047d7c3b43f9e3c
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-05-25 22:26:59 +0200

    changes to fit HB

--- src/components/nav/navBar.jsx
+++ src/components/nav/navBar.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./nav.css"; // Import CSS/LESS file directly
-import NaturalCritIcon from "./naturalcrit.svg.jsx";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css'; // Import CSS/LESS file directly
+import NaturalCritIcon from './naturalcrit.svg.jsx';
 
 const Nav = () => {
     return (
         <nav className="nav">
             <div className="navSection">
-                <div>
-                    <a className="navItem logo" href="https://www.naturalcrit.com">
-                        <NaturalCritIcon />
-                        <span className="name">
-                            Natural<span className="crit">Crit</span>
-                        </span>
-                    </a>
-                </div>
+                <a className="navItem logo" href="https://www.naturalcrit.com">
+                    <NaturalCritIcon />
+                    <span className="name">
+                        Natural<span className="crit">Crit</span>
+                    </span>
+                </a>
+
                 <a
                     className="navItem logo"
                     href="https://homebrewery.naturalcrit.com/"
